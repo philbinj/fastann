f1ed40900e57379769c6b33f4b3efc5079785042
James Philbin <philbinj@gmail.com>
2009-11-04 22:48:48 +0000

    Sped up the function a wee bit

--- dist_l2_funcs_exp.asm
+++ dist_l2_funcs_exp.asm
@@ -29,8 +29,7 @@ SECTION .text
 %define dsq_out [ebp + 0x18]
 ; Locals
 %define local_n     esi
-%define local_D     ebx
-%define local_D_8   edi
+%define local_dsq_out ebx
 %define local_d     ecx
 %define local_qu    eax
 %define local_pntn  edx
@@ -45,31 +44,27 @@ dl2v_2_8_exp:
     mov ebp, esp
     push esi
     push ebx
-    push edi
 
-    mov local_D, D
-    lea local_D, [local_D*8]    ; 8 bytes per double
-    mov local_D_8, D            
-    and local_D_8, -0x8         ; local_D = D&-8
-    lea local_D_8, [local_D_8*8]
-    
+    ; Quick bail for N=0
+    mov eax, N
+    test eax, eax
+    jz .all_zero
+
+    ; Quick bail for D=0
+
     mov local_pntn, pnts
+    mov local_dsq_out, dsq_out
 
-    xor local_n, local_n ; n = 0
-    jmp .end_N_loop
+    mov local_n, N
 .N_loop:
-    xor local_d, local_d ; d = 0
-
     mov local_qu, qu
     xorpd xmm4, xmm4     ; acc1 = 0.0
     xorpd xmm5, xmm5     ; acc2 = 0.0
     
-    jmp .end_D_8_loop
+    mov local_d, D
+    and local_d, -0x8
+    jmp near .end_D_8_loop
 .D_8_loop:
-;    movupd xmm0, [local_qu]
-;    movupd xmm1, [local_pntn]
-;    movupd xmm2, [local_qu + 2*8]
-;    movupd xmm3, [local_pntn + 2*8]
     movlpd xmm0, [local_qu]
     movhpd xmm0, [local_qu + 8]
     movlpd xmm1, [local_pntn]
@@ -102,48 +97,57 @@ dl2v_2_8_exp:
     addpd xmm4, xmm0
     addpd xmm5, xmm2
 
-    add local_d, 8*8
     add local_pntn, 8*8
     add local_qu, 8*8
+    sub local_d, 8
 .end_D_8_loop:
-    cmp local_d, local_D_8
-    jl near .D_8_loop
+    jnz near .D_8_loop
 
-    jmp near .end_D_loop
-.D_loop:
+    mov local_d, D
+    and local_d, 0x7
+    and local_d, -0x2
+    jmp near .end_D_2_loop
+.D_2_loop:
+    movlpd xmm0, [local_qu]
+    movhpd xmm0, [local_qu + 8]
+    movlpd xmm1, [local_pntn]
+    movhpd xmm1, [local_pntn + 8]
+    subpd xmm0, xmm1
+    mulpd xmm0, xmm0
+    addpd xmm4, xmm0
+
+    add local_qu, 2*8
+    add local_pntn, 2*8
+    sub local_d, 2
+.end_D_2_loop:
+    jnz near .D_2_loop
+
+    mov local_d, D
+    and local_d, 0x1
+    jz .end_D_loop
     movsd xmm0, [local_qu]
     movsd xmm1, [local_pntn]
     subsd xmm0, xmm1
     mulsd xmm0, xmm0
     addsd xmm4, xmm0
-    
-    add local_d, 8
-    add local_pntn, 8
     add local_qu, 8
+    add local_pntn, 8
+    
 .end_D_loop:
-    cmp local_d, local_D
-    jl near .D_loop
-
     ; Sum up the accumulators
     addpd xmm4, xmm5
     movapd xmm5, xmm4
     shufpd xmm5, xmm5, 0x1
     addsd xmm4, xmm5
 
-    ; Temporarily hijack local_D
-    mov local_D, dsq_out
-    movlpd [local_D], xmm4
-    add local_D, 8
-    mov dsq_out, local_D
-    mov local_D, D
-    lea local_D, [local_D*8]
+    movlpd [local_dsq_out], xmm4
+    add local_dsq_out, 8
     
-    add local_n, 0x1
+    sub local_n, 1
 .end_N_loop:
-    cmp local_n, N
-    jl .N_loop
-    
-    pop edi
+    jnz .N_loop
+
+.all_zero:
     pop ebx
     pop esi
     pop ebp
