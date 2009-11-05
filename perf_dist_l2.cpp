6ef79c5172c5e69b2c0b84e5c07aa755e5b961e5
James Philbin <philbinj@gmail.com>
2009-11-05 16:09:10 +0000

    Bugfix for 64-bit

--- perf_dist_l2.cpp
+++ perf_dist_l2.cpp
@@ -98,7 +98,9 @@ perf(int N, int D)
         { &dl2f_1_8, "dl2f_1_8" },
 #ifdef __SSE2__
         { &dl2v_2_8, "dl2v_2_8" },
+#ifdef EXPERIMENTAL_ASM
         { &dl2v_2_8_exp, "dl2v_2_8_exp" },
+#endif
 #endif
     };
     
